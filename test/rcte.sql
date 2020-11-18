cbaa479c7906606462aaf6581acb792daaed74e2
abetlen <abetlen@gmail.com>
2020-11-18 01:35:11 -0500

    Update test to include paths

--- test/rcte.sql
+++ test/rcte.sql
@@ -1,13 +1,13 @@
 .read ./test/fixture.sql
 with recursive
-    bfs(id, parent, distance) as (
-        select 1, null, 0
+    bfs(id, parent, path, distance) as (
+        select 1, null, '/' || 1 || '/',  0
         union all
-        select edges.toNode, bfs.id, bfs.distance + 1
+        select edges.toNode, bfs.id, bfs.path || edges.toNode || '/', bfs.distance + 1
         from edges, bfs
         where edges.fromNode = bfs.id
         order by 2
     )
-select id, parent, min(distance) as distance from bfs
+select id, parent, path, min(distance) as distance from bfs
 group by id
 order by distance;
